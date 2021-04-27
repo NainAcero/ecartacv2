f8ec927bdcf1b311a574c5a8443a771cca0fe00a
William Calisaya <>
2021-04-27 13:04:56 -0500

    holia

--- resources/views/frontend/filtrados/productotienda.blade.php
+++ resources/views/frontend/filtrados/productotienda.blade.php
@@ -35,7 +35,7 @@
 
 @endsection
 @section('scripts')
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app1.js') }}" defer></script>
     <script src="https://bootstrap-ecommerce.com/bootstrap-ecommerce-html/plugins/owlcarousel/owl.carousel.min.js"></script>
     <script>
         $("#owl-example").owlCarousel({
