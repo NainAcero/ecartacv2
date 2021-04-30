9be4cbe6bcb0e7e8312b99d157f7d11b943bdecc
NainAceroMamani <a_nacerom@unjbg.edu.pe>
2021-04-30 11:54:53 -0500

    update

--- resources/views/frontend/feria/isometrica.blade.php
+++ resources/views/frontend/feria/isometrica.blade.php
@@ -46,10 +46,7 @@
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen></iframe> --}}
 
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/WkrhVexll_g?controls=0"
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
+                    <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FMunicipalidadProvincialDeTacna%2Fvideos%2F498352388145017%2F&show_text=false&width=560" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
 
                   <div class="text-center mt-5" hidden>
                     <a class="btn black " href="https://meet.google.com/pup-veda-bqy">
