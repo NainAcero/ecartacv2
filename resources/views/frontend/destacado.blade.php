7c300ec9c444991b7325a5ff52100306bbe8906c
William Calisaya <>
2021-05-03 17:55:41 -0500

    fix search produc final de la final 4

--- resources/views/frontend/destacado.blade.php
+++ resources/views/frontend/destacado.blade.php
@@ -32,7 +32,9 @@
                         <small><p class="text-muted">{{$item->ingredientes}}</p></small>
                     </a>
 
-                    <span class="label-rating text-warning"><a href="{{url('r/'.$item->tienda->slug)}}"  style="color: royalblue">{{$item->tienda->tienda}}</a></span>
+                    <small>
+                        <span class="label-rating text-warning"><a href="{{url('r/'.$item->tienda->slug)}}"  style="color: royalblue">{{$item->tienda->tienda}}</a></span>
+                    </small>
                     {{-- <dd class="col-sm-10"><a href="{{url('r/'.$item->tienda->slug)}}"  style="color: royalblue">{{$item->tienda->tienda}}</a></dd> --}}
 
                 </figcaption>
