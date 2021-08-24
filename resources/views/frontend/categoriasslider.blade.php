c22bc51479a4da7d6d312b19fd54b593dfb0b31b
William Calisaya <>
2021-08-24 17:05:50 -0500

    feria tacna4

--- resources/views/frontend/categoriasslider.blade.php
+++ resources/views/frontend/categoriasslider.blade.php
@@ -6,7 +6,7 @@
           <img src="{{asset('ecom/images/banners/slide-lg-122.png')}}" alt="First slide">
         </div> --}}
         <div class="carousel-item active">
-          <img src="{{asset('ecom/images/banners/slide-lg-10.jpg')}}" alt="First slide">
+          <img src="{{asset('ecom/images/banners/slide_expoferia.png')}}" alt="First slide">
         </div>
         {{-- <div class="carousel-item">
             <img src="{{asset('ecom/images/banners/slide-lg-4.jpg')}}" alt="Second slide">
@@ -14,7 +14,7 @@
       {{-- <div class="carousel-item">
             <img src="{{asset('ecom/images/banners/slide-lg-3.jpg')}}" alt="Third slide">
     </div> --}}
-    {{-- <a href="{{url('feriadigital')}}" class="btn btn-primary btn-lg btn-feria">Ingresar a la Feria</a> --}}
+    <a href="{{url('feriadigital')}}" class="btn btn-primary btn-lg btn-feria">Ingresar a la Feria</a>
   </div>
   </div>
   </div>
